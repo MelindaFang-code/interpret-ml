e0d127d5cf0c9a09c001bb161de06d94324b8b10
Winnie Chow <wc459@cornell.edu>
2021-10-04 10:26:04 -0400

    Implement sort of working additive model

--- gam.py
+++ gam.py
@@ -2,7 +2,6 @@
 from os import name
 import dataset
 data = dataset.load_dataset('taiwan_credit_risk', 0.7, 0.3)
-print(data.X_train)
 # %%
 import numpy as np
 import matplotlib.pyplot as plt
@@ -15,6 +14,13 @@ x = (size - min(size))
 x /= np.max(x)
 
 # %%
+
+
+def mat_sqrt(X):
+    ''' Returns square root of a matrix by eigen decomposition'''
+    w, v = np.linalg.eig(X)
+    D = np.diag(w ** 0.5)
+    return v @ D @ v.T #inverse equals transpose for orthogonal matrix
 class Spline:
 
     def __init__(self, x, xk):
@@ -39,12 +45,6 @@ class PRSpline(Spline):
     def __init__(self, x, xk):
         super().__init__(x, xk)
 
-    @staticmethod
-    def mat_sqrt(X):
-        ''' Returns square root of a matrix by eigen decomposition'''
-        w, v = np.linalg.eig(X)
-        D = np.diag(w ** 0.5)
-        return v @ D @ v.T #inverse equals transpose for orthogonal matrix
     
     def S(self):
         ''' Set up the penalized regression spline penalty matrix'''
@@ -54,7 +54,7 @@ class PRSpline(Spline):
     
     def prs_matrix(self, lmbda):
         ''' Returns the penalized regression spline matrix'''
-        return np.concatenate((self.X(), (PRSpline.mat_sqrt(self.S())*(lmbda**0.5))), axis=0)
+        return np.concatenate((self.X(), (mat_sqrt(self.S())*(lmbda**0.5))), axis=0)
     
 def lin_reg(X, y):
     ''' Return linear model'''
@@ -81,6 +81,74 @@ class MyLinearRegression:
 
     def predict(self, X):
         return X @ self.b_hat
+
+
+class AdditiveModel:
+    ''' Additive Model for multiple covariates'''
+    
+    def __init__(self, X_in, lmdas, q=10):
+        self.X_in = X_in # covariate matrix
+        self.n = X_in.shape[0] # number of data
+        self.p = X_in.shape[1] # number of covariates
+        self.q = q # number of knots?
+        # self.X = None # model matrix
+        # self.S_list = [] # list of penalty matrices
+        self.lmdas = lmdas # list of smoothing parameters
+        self.lm = None
+    
+    def get_X_S_list(self, X_in):
+        ''' Get X and list of S for a simple AM'''
+        i = self.p - 1
+        j = self.q
+        num_col = self.p * self.q - (self.p-1)
+        X = np.ones((self.n , num_col))
+        S_list = []
+
+        for k in range(self.p):
+
+            x = X_in[:, k]
+            # Choose knots
+            xk = np.quantile(np.unique(x), np.arange(1, self.q - 1) / (self.q -1))# TODO: not sure why unique is needed
+            
+            # Get penalty matrices
+            S = np.zeros((num_col, num_col))  
+            spl = PRSpline(x, xk)
+            S[i:j, i:j] = spl.S()[1:, 1:] # drop rows and columns corresponding to the intercept of each term
+            S_list.append(S)
+
+            # Get model matrix
+            X[:, i:j] = spl.X()[:, 1:]
+
+            i = j
+            j += self.q - 1
+
+        return X, S_list
+
+    def augment_X(self, X, S_list):
+        S = np.empty(S_list[0].shape)
+        for Si, lmda in zip(S_list, self.lmdas):
+            S += lmda * Si
+         
+        rS = mat_sqrt(S) # Get sqrt of total penalty matrix
+        return np.concatenate((X, rS), axis=0)
+
+    def fit(self, y):
+        ''' lmdas is a list of smoothing parameters'''
+        # Get model matrix and list of penalty matrix
+        X, S_list = self.get_X_S_list(self.X_in)
+       
+        X1 = self.augment_X(X, S_list)
+        q = X.shape[1] # number of parameter in the processed model matrix
+        y1 = np.concatenate((y.reshape(-1,1), np.zeros((q, 1))), axis=0)
+        self.lm = MyLinearRegression(X1, y1)
+        self.lm.fit()
+
+    def predict(self, Xp):
+        n = Xp.shape[0]
+        X, S_list = self.get_X_S_list(Xp)
+        X1 = self.augment_X(X, S_list)
+        return self.lm.predict(X1)[:n, :]
+
 
 xk = np.arange(1,5) / 5
 spl = Spline(x, xk)
@@ -114,5 +182,34 @@ plt.plot(x, wear, '.')
 pred = lm_prs.predict(Xp)
 plt.plot(xp, pred[:-spl_p.q], '.')
 
+
+# %% Additive Model
+import pandas as pd
+data = pd.read_csv('trees.csv', index_col=0).to_numpy()
+X = data[:,:-1]
+y = data[:,-1]
+# Rescale predictors onto [0,1]
+
+class Scaler:
+    
+    def __init__(self, X):
+        self.min = np.min(X)
+        self.max = np.max(X)
+    
+    def transform(self, X):
+        return (X - self.min) / (self.max - self.min)
+
+    def rev_transform(self, X_s):
+        return X_s * (self.max - self.min) + self.min
+
+scaler_X = Scaler(X)
+scaler_y = Scaler(y)
+X_s = scaler_X.transform(X)
+y_s = scaler_y.transform(y)
+
+lmdas = [0.01024, 5368.70912]
+am = AdditiveModel(X_s, lmdas, 10)
+am.fit(y_s)
+plt.plot(y, scaler_y.rev_transform(am.predict(X_s)), '.')
 
 # %%
